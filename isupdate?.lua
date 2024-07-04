5805d424698871a3c4c281c0fc75711001334fd5
Oproxide <coopzek@gmail.com>
2024-07-04 02:16:01 -0400

    Update isupdate?.lua

--- isupdate?.lua
+++ isupdate?.lua
@@ -1,5 +1,5 @@
-local update = false
+local update = true
 
 if update == true then
-    game.Players.LocalPlayer:Kick("https://discord.gg/GJS3mCDCgk \n Join for update")
+    game.Players.LocalPlayer:Kick("Join for update\nhttps://discord.gg/GJS3mCDCgk")
 end
