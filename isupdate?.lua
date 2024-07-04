3a776795751db34f2c335752817dc2c1139bc1bf
Oproxide <coopzek@gmail.com>
2024-07-04 02:19:51 -0400

    Update isupdate?.lua

--- isupdate?.lua
+++ isupdate?.lua
@@ -1,5 +1,10 @@
-local update = true
+local update = false
 
 if update == true then
-    game.Players.LocalPlayer:Kick("Join for update\nhttps://discord.gg/GJS3mCDCgk")
+    game.Players.LocalPlayer:Kick("Join for update   https://discord.gg/GJS3mCDCgk")
+else
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Newest verison required!", -- Required
+	Text = "V1 newest verison", -- Required
+})
 end
